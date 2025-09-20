3c6bee90bcd9808ba3c839776000a76ae014d6de
Raja Shekar <rajashekardg444@gmail.com>
2025-09-20 10:27:01 +0000

    use this newsdata.io api key : pub_3e231d27d02b413a804e6216d1b83058

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 export const firebaseConfig = {
-  projectId: "karnataka-news-buzz",
-  appId: "1:298153641015:web:c285db9ada95a1fbe22bb2",
-  storageBucket: "karnataka-news-buzz.firebasestorage.app",
-  apiKey: "AIzaSyDX_GFsHRhloy9ZceOfEcnFpIX4WiBHf38",
-  authDomain: "karnataka-news-buzz.firebaseapp.com",
-  messagingSenderId: "298153641015",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 // Initialize Firebase
